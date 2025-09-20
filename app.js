0a5dd0e3dedc335fccab13699504ba41ce4dff7d
Sandipan Jha <sandipanjha3@gmail.com>
2025-09-20 15:40:19 +0530

    hmm

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 //======localhost========
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-//=========================
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+//=========================
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/auth", authRouter);
 app.use("/api/vote", voteRouter);
